423763c850bf3aa06cf9049110fa58773e3d9355
aspen-yryr <aspen_development@outlook.com>
2023-01-22 05:40:30 +0900

    Fix: ボリュームのマウント 専用のボリュームに取得データを保存する

--- scripts/fetch.py
+++ scripts/fetch.py
@@ -1,3 +1,4 @@
+import io
 import os
 
 import datetime as dt
@@ -5,16 +6,13 @@ import abc
 import requests
 import logging
 import zipfile
-
-LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
-logging.basicConfig(level=LOGLEVEL)
+import multiprocessing
 
 
 class Api(abc.ABC):
-    BASE_URI = "https://api.daiwa-open-challenge.jp/files"
-
-    def __init__(self, key: str) -> None:
+    def __init__(self, key: str, base_uri: str) -> None:
         self.key = key
+        self.base_uri = base_uri
 
     @property
     @abc.abstractmethod
@@ -23,45 +21,79 @@ class Api(abc.ABC):
 
     def get(self, date: dt.datetime):
         params = {"acl:consumerKey": self.key}
-        url = (
-            self.BASE_URI
-            + "/"
-            + self.resource_name
-            + "/"
-            + self.resource_name
-            + "_"
-            + date.strftime("%Y%m%d")
-            + ".zip"
-        )
-        res = requests.get(url, params=params, stream=True, timeout=(10.0, 60.0))
+        zip_name = self.resource_name + "_" + date.strftime("%Y%m%d") + ".zip"
 
-        filename = url.split("/")[-1]
-        with open("./data/" + filename, "wb") as f:
-            for chunk in res.iter_content(chunk_size=1024):
+        logging.info("fetching " + zip_name)
+
+        res = requests.get(
+            self.base_uri + "/" + self.resource_name + "/" + zip_name,
+            params=params,
+            stream=True,
+            timeout=(10.0, 60.0),
+        )
+        res.raise_for_status()
+
+        with open("./data/" + zip_name, "wb") as f:
+            for chunk in res.iter_content(chunk_size=1024 * 16):
                 if chunk:
                     f.write(chunk)
                     f.flush()
 
-        with zipfile.ZipFile("./data/" + filename) as zf:
-            zf.extractall("./data/" + filename.rsplit(".zip", 1)[0])
+        logging.info("fetched " + zip_name)
 
-        os.remove("./data/" + filename)
+        def async_extract():
+            with zipfile.ZipFile("./data/" + zip_name) as zf:
+                for info in zf.infolist():
+                    zf.extract(info, path="./data/" + zip_name.rsplit(".zip", 1)[0])
+
+            os.remove("./data/" + zip_name)
+            logging.info("extracted " + zip_name)
+
+        multiprocessing.Process(target=async_extract).start()
+
+
+class VitalApi(Api):
+    resource_name = "vitals"  # type: ignore
 
 
 class EventApi(Api):
     resource_name = "events"  # type: ignore
 
 
+class PositionApi(Api):
+    resource_name = "sensors_map"  # type: ignore
+
+
 class SensorAccApi(Api):
     resource_name = "sensors_acc"  # type: ignore
+
+
+class DrivingVitalApi(Api):
+    resource_name = "driving_vitals"  # type: ignore
 
 
 if __name__ == "__main__":
     from dotenv import load_dotenv
 
-    load_dotenv("./env/.env.dev")
-    event_api = EventApi(os.environ["API_KEY"])
-    event_api.get(date=dt.datetime(2022, 1, 1))
+    BASE_URI = "https://api.daiwa-open-challenge.jp/files"
 
-    sensor_api = SensorAccApi(os.environ["API_KEY"])
-    sensor_api.get(date=dt.datetime(2022, 9, 4))
+    LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
+    logging.basicConfig(level=LOGLEVEL)
+
+    load_dotenv("./env/.env.dev")
+
+    vital_api = VitalApi(os.environ["API_KEY"], BASE_URI)
+    event_api = EventApi(os.environ["API_KEY"], BASE_URI)
+    sensor_api = SensorAccApi(os.environ["API_KEY"], BASE_URI)
+    position_api = PositionApi(os.environ["API_KEY"], BASE_URI)
+    driving_vital_api = DrivingVitalApi(os.environ["API_KEY"], BASE_URI)
+
+    date = dt.datetime(2022, 1, 1)
+    while date < dt.datetime(2022, 12, 1):
+        for api in [vital_api, event_api, sensor_api, position_api, driving_vital_api]:
+            try:
+                api.get(date)
+            except:
+                logging.error(str(api.__class__) + " error: " + date.strftime("%Y%m%d"))
+
+        date = date + dt.timedelta(days=1)
