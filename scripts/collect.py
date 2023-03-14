9fdfc6cf0a5706dbbde5be5ef1151ee02b32586d
Aspen_yryr <aspen_development@outlook.com>
2023-03-14 15:56:31 +0000

    Fix: ディレクトリ整理スクリプト csvのディレクトリ移動を共通化．pathlibの導入

--- scripts/collect.py
+++ scripts/collect.py
@@ -1,12 +1,27 @@
-import logging
 import os
 import shutil
+from logging import getLogger
+from pathlib import Path
 
-for dir in os.listdir("./data"):
-    if not dir.startswith("vitals_"):
-        continue
-    for file in os.listdir("./data/" + dir):
-        if file.endswith(".csv"):
-            shutil.move("./data/" + dir + "/" + file, "./data/vitals/")
-        else:
-            logging.warning("skipped file: " + "./data/" + dir + "/" + file)
+logger = getLogger(__name__)
+logger.setLevel(os.getenv("LOG_LEVEL", "WARNING"))
+
+BASE_DIR = Path("./data").resolve()
+
+
+def collect(resource: str):
+    for dir in BASE_DIR.iterdir():
+        if not dir.name.startswith(resource + "_") or dir.is_file():
+            continue
+        for file in dir.iterdir():
+            if file.name.endswith(".csv"):
+                shutil.move(file, BASE_DIR / resource / file.name)
+                logger.debug("processed file: " + file.name)
+            else:
+                logger.warning("skipped file: " + file.name)
+
+
+resources = ["vitals", "events", "sensors_acc", "sensors_map", "driving_vitals"]
+
+for r in resources:
+    collect(r)
