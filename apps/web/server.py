a55ea2f45e9f955c0ce70b2e2399895fa0b6064a
Aspen_yryr <aspen_development@outlook.com>
2023-08-12 19:30:24 +0900

    #8 ユーザー情報の入力追加

--- apps/web/server.py
+++ apps/web/server.py
@@ -1,30 +1,41 @@
+import infer
 import streamlit as st
 
-from pycaret.datasets import get_data
-from pycaret.regression import *
-
-data = get_data("boston")
 
 @st.cache_resource()
-def create_model_cache(estimator):
-    return create_model(estimator)
-
-number = st.number_input('Insert a number')
-st.write('The current number is ', number)
-
-pipe = setup(data, target="medv", html=False)
-
-@st.cache_resource()
-def ensemble_model_cache(estimators):
-    _model = blend_models(estimator_list=[create_model_cache(estimator) for estimator in estimators])
-    return ensemble_model(_model)
-
-model = ensemble_model_cache(["et", "lightgbm"])
-
-plot_model(model, plot="cooks", display_format="streamlit")
-
-predictions = predict_model(model)
-st.write(predictions)
+def get_model():
+    return infer.Model(cfg={"file": "./models/save_test"})
 
 
+model = get_model()
 
+age = st.number_input("Age", min_value=25, max_value=69, format="%d")
+sex = st.selectbox("年齢", ("男", "女"), index=0) or "男"
+
+
+# plot_model(model, plot="cooks", display_format="streamlit")
+
+# predictions = predict_model(model)
+data: infer.ModelInput = {
+    "car": {
+        "car_load": [1600],
+        "max_load": [0],
+        "load": [2040],
+        "depth": [468],
+        "width": [169],
+        "height": [186],
+        "ventilation": [3.99],
+    },
+    "user": {"age": [int(age)], "sex": [sex]},
+    "vital": {
+        "time_hour": [5],
+        "body_temp": [37.5],
+        "spo2": [98.0],
+        "sys": [140.0],
+        "dia": [96.0],
+        "fatigue_lh": [2.06],
+        "fatigue_deviation": [70.0],
+    },
+}
+score = model(data)
+st.write(score)
